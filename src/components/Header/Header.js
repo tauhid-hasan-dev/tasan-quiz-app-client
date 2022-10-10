0aa217312e7aace02e444e05136e6e8bcfc17edd
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-10 19:38:17 +0300

    navbar added and fontawesome added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBurger } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <div>
-            <p>this is header</p>
-        </div>
+        <div className="navbar bg-navbar-color  shadow-md px-5  lg:px-20 py-5 ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden text-white text-6xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <NavLink
+                    to="/home"
+                    className={({ isActive }) =>
+              isActive ? 'text-orange-400 border-b-2 border-logo-color': undefined
+            }
+                    >Home</NavLink>
+
+                            <NavLink to='/statistics' className={({ isActive }) =>
+              isActive ? 'text-orange-400 border-b-2 border-logo-color': undefined
+            } >Statistics</NavLink>
+
+                            <NavLink to='/blog' className={({ isActive }) =>
+              isActive ? 'text-orange-400 border-b-2 border-logo-color': undefined
+            }>Blog</NavLink>
+                        </ul>
+                    </div>
+                    <a href=" " className="btn btn-ghost normal-case text-4xl text-logo-color font-bold ">foody</a>
+                </div>
+                <div className="navbar-center hidden lg:flex text-6xl text-logo-text-color  ">
+                    <FontAwesomeIcon icon={faBurger} />
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0 text-slate-100 gap-5 text-lg ">
+                    <NavLink
+                    to="/home"
+                    className={({ isActive }) =>
+              isActive ? 'text-orange-400 border-b-2 border-logo-color': undefined
+            }
+                    >Home</NavLink>
+
+                            <NavLink to='/statistics' className={({ isActive }) =>
+              isActive ? 'text-orange-400 border-b-2 border-logo-color': undefined
+            } >Statistics</NavLink>
+
+                            <NavLink to='/blog' className={({ isActive }) =>
+              isActive ? 'text-orange-400 border-b-2 border-logo-color': undefined
+            }>Blog</NavLink>
+
+                            
+                    </ul>
+                </div> 
+            </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navbar-color': '#0B1222',
+      },
+    },
+
   },
   plugins: [require("daisyui")],
 }
