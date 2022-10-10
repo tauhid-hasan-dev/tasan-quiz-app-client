2c580557ecd9e1f99346f042d16811f4880fdb78
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-11 01:21:38 +0300

    if answer is correct the show a notification or alert done

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Option = ({option}) => {
+const Option = ({option, correctAnswer}) => {
+    const [isActive, setIsActive] = useState(false);
+    const handleClick = () => {
+        if(option === correctAnswer){
+            alert('correct')
+            setIsActive(current => !current);
+        }
+      };
+    console.log(correctAnswer, option);
     
     return (
-        <div className='text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900'>
+        <div className={`text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900 ${isActive ? 'bg-cyan-900' : ''}`} onClick={handleClick}>
             <li>{option}</li>
         </div>
     );
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,6 +3,7 @@ import Option from '../Option/Option';
 
 const Question = ({data, idx, total}) => {
     const {correctAnswer, options, question} = data;
+    console.log(data)
     return (
         <div className='text-white border border-slate-800 bg-card-bg-color mt-10 flex lg:flex-row flex-col gap-10 p-10 rounded-3xl'>
             <div className='w-full lg:w-[60%] flex flex-col gap-10 text-center lg:text-left  '>
@@ -12,7 +13,7 @@ const Question = ({data, idx, total}) => {
             <div className='w-full lg:w-[40%] '>
                  <ul className='flex flex-col gap-5'>
                     {
-                        options.map((option, idx) => <Option key={idx} option={option}></Option>)
+                        options.map((option, idx) => <Option key={idx} option={option} correctAnswer={correctAnswer}></Option>)
                     }
                  </ul>
             </div>
