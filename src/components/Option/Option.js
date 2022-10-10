f3d188a935a6551e27b990aebc44060d28cb06d3
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-11 01:42:14 +0300

    added radio to the anser option

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-
 const Option = ({option, correctAnswer}) => {
-    const [isActive, setIsActive] = useState(false);
+    
     const handleClick = () => {
         if(option === correctAnswer){
             alert('correct')
-            setIsActive(current => !current);
         }
       };
-    console.log(correctAnswer, option);
     
     return (
-        <div className={`text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900 ${isActive ? 'bg-cyan-900' : ''}`} onClick={handleClick}>
-            <li>{option}</li>
+        <div className={`text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900`} onClick={handleClick}>
+            <li className="form-control">
+                
+                    <label className="label cursor-pointer">
+                        <input onChange={handleClick} defaultChecked={true} type="radio" name="radio-6" className="radio checked:bg-blue-500 bg-white mr-3 " checked />
+                        <span className="label-text text-white text-2xl">{option}</span> 
+                    </label>
+                
+            </li>
         </div>
     );
 };
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -6,11 +6,11 @@ const Question = ({data, idx, total}) => {
     console.log(data)
     return (
         <div className='text-white border border-slate-800 bg-card-bg-color mt-10 flex lg:flex-row flex-col gap-10 p-10 rounded-3xl'>
-            <div className='w-full lg:w-[60%] flex flex-col gap-10 text-center lg:text-left  '>
+            <div className='w-full lg:w-[55%] flex flex-col gap-10 text-center lg:text-left  '>
                 <p className='text-3xl font-semibold text-slate-400'> Question {idx + 1}<span className='text-xl text-slate-700'>/{total}</span> </p>
                 <p className='text-lg lg:text-2xl'>{question}</p>
             </div>
-            <div className='w-full lg:w-[40%] '>
+            <div className='w-full lg:w-[45%] '>
                  <ul className='flex flex-col gap-5'>
                     {
                         options.map((option, idx) => <Option key={idx} option={option} correctAnswer={correctAnswer}></Option>)
