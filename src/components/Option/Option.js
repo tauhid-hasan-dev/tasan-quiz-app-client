1b93fb8093f45030f1cf03c6b60a075dbfac16b6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-11 02:22:55 +0300

    toastify added when the answer is true

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,20 +1,21 @@
+import Swal from 'sweetalert2'
+
 const Option = ({option, correctAnswer}) => {
     
     const handleClick = () => {
         if(option === correctAnswer){
-            alert('correct')
+            Swal.fire(
+                'Correct Answer!',
+                ` ${correctAnswer}`,
+                'success'
+              )
         }
       };
     
     return (
-        <div className={`text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900`} onClick={handleClick}>
-            <li className="form-control">
-                
-                    <label className="label cursor-pointer">
-                        <input onChange={handleClick} defaultChecked={true} type="radio" name="radio-6" className="radio checked:bg-blue-500 bg-white mr-3 " checked />
-                        <span className="label-text text-white text-2xl">{option}</span> 
-                    </label>
-                
+        <div className={`text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900`}  onClick={handleClick}>
+            <li className="form-control" >
+                <span className="label-text text-white text-2xl">{option}</span> 
             </li>
         </div>
     );
