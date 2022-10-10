3eca052d0ed68b992be41dfb9caa70c34a91197b
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-10 19:24:16 +0300

    added not found route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog/Blog";
 import Home from "../components/Home/Home";
+import NotFound from "../components/NotFound/NotFound";
 import Statistics from "../components/Statistics/Statistics";
 import Main from "../layouts/Main";
 
@@ -13,5 +14,7 @@ export const router = createBrowserRouter([
         {path:'/statistics', element: <Statistics></Statistics> },
         {path:'/blog', element: <Blog></Blog> },
     ]
-}
+    }, 
+    {path:'*', element: <NotFound></NotFound>}
+
 ])
