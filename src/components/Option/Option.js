a58da176c3a968540a9fa2c18ddaa2f902de7f18
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-11 00:41:56 +0300

    quiz questions style updated

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Option = ({option}) => {
     
     return (
-        <div className='text-xl border border-blue-500 flex p-4 rounded-lg cursor-pointer hover:bg-cyan-900'>
+        <div className='text-lg lg:text-xl  border lg:border-2  border-blue-500 flex lg:p-4 p-2  rounded-lg cursor-pointer hover:bg-cyan-900'>
             <li>{option}</li>
         </div>
     );
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -8,7 +8,7 @@ const Quiz = () => {
     const datas = quizOptions.data.questions;
     console.log(datas)
     return (
-        <div className='bg-card-color px-5 lg:px-28 py-16  '>
+        <div className='bg-card-color px-5 lg:px-28 py-10'>
             <p className='text-white text-3xl text-center'>Quiz of <span className='text-3xl font-semibold text-blue-color'>{name}</span></p>
             <div>
                 {
