1c341b6f6270126829581f5af1018aa7711390e3
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-11 14:15:21 +0300

    eye style updated

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -17,7 +17,7 @@ const Question = ({data, idx, total}) => {
             <div className='text-white border border-slate-800 bg-card-bg-color mt-10 flex lg:flex-row flex-col gap-10 p-10 rounded-3xl'>
                 
                 <div className='w-full lg:w-[55%] flex flex-col gap-10 text-center lg:text-left  '>
-                    <span className='text-2xl cursor-pointer hover:text-blue-color' title='Click to see correct ans' onClick={handleCorrectAns}>  <FontAwesomeIcon icon={faEye} /> </span>
+                    <span className='text-2xl cursor-pointer hover:text-blue-color' title='Click to see correct ans' onClick={handleCorrectAns}>  <FontAwesomeIcon className='hover:text-blue-color' icon={faEye} />  </span>
                     <p className='text-3xl font-semibold text-slate-400'>  Question {idx + 1}<span className='text-xl text-slate-700'>/{total}</span> </p>
                     <p className='text-lg lg:text-2xl'>{question}</p>
                 </div>
