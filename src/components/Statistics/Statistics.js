318f33a711684b75d4747f2fd5ac74536d0bd213
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-11 16:03:33 +0300

    responsiveness done in recharts

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
     const datas = useLoaderData();
     const data = datas.data;
     return (
-        <div className='flex flex-col justify-center items-center lg:py-10'>
-            <p className='text-2xl font-semibold text-center py-5'>Total Questions Statistics</p>
-            <div>
-                <LineChart
-                        width={1000}
-                        height={400}
-                        data={data} > 
-                        <CartesianGrid strokeDasharray="3 3" />
-                            <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                </LineChart>
+        <div className='flex flex-col justify-center items-center lg:py-10 bg-slate-900 '>
+            <p className='text-2xl font-semibold text-center py-10'>Total Questions Statistics</p>
+            <div className='lg:w-[50%] w-[100%] h-64 pl-5 flex flex-col items-center p-20 justify-center'>
+                <ResponsiveContainer width="99%" height={300}>
+                    <LineChart
+                            width={1000}
+                            height={400}
+                            data={data} > 
+                            <CartesianGrid strokeDasharray="3 3" />
+                                <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
